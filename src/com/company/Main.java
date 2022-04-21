8830948614ea9a2ccf573dd1a0f67fcc4a4b872d
Jaide Edwards <jaideedwards@jaides-air.myfiosgateway.com>
2022-04-21 17:27:50 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+import  java.lang.String;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// package.company --> package com.company;
+        // import.util.java*; --> import java.util.[insert util here];
+        // import.java.lang*; --> import java.lang.;
+        // import.java.io*; --> import java.io.;
+
+         {
+            //Block of  code goes here.
+            Scanner cool = new Scanner(System.in);
+            System.out.println("Hello user. How are you today? ");
+            cool.nextLine();
+
+
+            }
+
+}}
